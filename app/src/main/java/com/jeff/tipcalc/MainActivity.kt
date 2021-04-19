d6f2432245c529b5496c19bc3c33efe241dd24f7
tse-jeff <jtse0316@gmail.com>
2021-04-19 17:24:24 -0400

    finished tip calculator functions

--- app/src/main/java/com/jeff/tipcalc/MainActivity.kt
+++ app/src/main/java/com/jeff/tipcalc/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jeff.tipcalc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jeff.tipcalc.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    /*
+    Calculates the tip based on the service cost
+     */
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDouble()
+
+        //determine tip percent from radio buttons
+        val tipPercent = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> .2
+            R.id.option_eighteen_percent -> .18
+            else -> .15
+        }
+        var tip = cost * tipPercent     //normal tip
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip, formattedTip)
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(tip+cost)
+        binding.totalAmount.text = getString(R.string.total_amount, formattedTotal)
+
     }
 }
